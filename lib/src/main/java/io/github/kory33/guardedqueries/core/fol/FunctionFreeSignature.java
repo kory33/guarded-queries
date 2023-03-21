1e1b5d2592e01b26c40ff685c6ed5ed9a1dc42dc
Kory <korygm33@gmail.com>
2023-03-21 22:58:50 +0000

    feat: define the model of function-free signatures

---
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/fol/FunctionFreeSignature.java
@@ -0,0 +1,30 @@
+package io.github.kory33.guardedqueries.core.fol;
+
+import com.google.common.collect.ImmutableSet;
+import io.github.kory33.guardedqueries.core.utils.FormulaExtra;
+import uk.ac.ox.cs.pdq.fol.Formula;
+import uk.ac.ox.cs.pdq.fol.Predicate;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * An object of this class represents a first-order logic signature with
+ * - countably-infinitely many constants
+ * - finite set of predicate symbols
+ * - no function symbols
+ */
+public record FunctionFreeSignature(ImmutableSet<Predicate> predicates) {
+    public FunctionFreeSignature(Iterable<? extends Predicate> predicates) {
+        this(ImmutableSet.copyOf(predicates));
+    }
+
+    public static FunctionFreeSignature fromFormulas(final Collection<Formula> formulas) {
+        return new FunctionFreeSignature(
+                formulas
+                        .stream()
+                        .flatMap(FormulaExtra::predicatesAppearingIn)
+                        .collect(Collectors.toList())
+        );
+    }
+}
---
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/utils/FormulaExtra.java
@@ -0,0 +1,26 @@
+package io.github.kory33.guardedqueries.core.utils;
+
+import uk.ac.ox.cs.pdq.fol.Atom;
+import uk.ac.ox.cs.pdq.fol.Formula;
+import uk.ac.ox.cs.pdq.fol.Predicate;
+
+import java.util.stream.Stream;
+
+public class FormulaExtra {
+    private FormulaExtra() {
+    }
+
+    public static Stream<Predicate> predicatesAppearingIn(final Formula formula) {
+        final var children = formula.getChildren();
+
+        if (children.length == 0) {
+            if (formula instanceof Atom) {
+                return Stream.of(((Atom) formula).getPredicate());
+            } else {
+                return Stream.empty();
+            }
+        } else {
+            return Stream.of(children).flatMap(FormulaExtra::predicatesAppearingIn);
+        }
+    }
+}
