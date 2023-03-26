e19126e16642837a9a888280e109fbdf6469ba50
Kory <korygm33@gmail.com>
2023-03-26 11:25:50 +0100

    feat: implement the computation of `subgoalGlueingRules`

--- lib/src/main/java/io/github/kory33/guardedqueries/core/rewriting/GuardedRuleAndQueryRewriter.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/rewriting/GuardedRuleAndQueryRewriter.java
@@ -1,35 +1,33 @@
 package io.github.kory33.guardedqueries.core.rewriting;
 
 import com.google.common.collect.ImmutableCollection;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import io.github.kory33.guardedqueries.core.datalog.DatalogProgram;
 import io.github.kory33.guardedqueries.core.datalog.DatalogQuery;
+import io.github.kory33.guardedqueries.core.fol.DatalogRule;
 import io.github.kory33.guardedqueries.core.fol.FunctionFreeSignature;
 import io.github.kory33.guardedqueries.core.fol.NormalGTGD;
-import io.github.kory33.guardedqueries.core.utils.extensions.StreamExtensions;
-import io.github.kory33.guardedqueries.core.utils.extensions.StringSetExtensions;
+import io.github.kory33.guardedqueries.core.utils.CachingFunction;
+import io.github.kory33.guardedqueries.core.utils.extensions.*;
 import uk.ac.ox.cs.gsat.AbstractSaturation;
 import uk.ac.ox.cs.gsat.GTGD;
 import uk.ac.ox.cs.pdq.fol.*;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public record GuardedRuleAndQueryRewriter(AbstractSaturation<? extends GTGD> saturation) {
     private record BoundVariableConnectedComponentRewriteResult(
             ImmutableCollection<? extends TGD> additionalRules,
-            Predicate goalPredicate,
-            ImmutableList<Variable> goalPredicateCanonicalVariables
+            Atom goalAtom
     ) {
-        public Atom goalAtom() {
-            return Atom.create(goalPredicate, goalPredicateCanonicalVariables.toArray(Term[]::new));
-        }
     }
 
     private BoundVariableConnectedComponentRewriteResult rewriteBoundVariableConnectedComponent(
             final SaturatedRuleSet<? extends NormalGTGD> saturatedRules,
-            final ConjunctiveQuery boundVariableConnectedSubquery,
+            final /* bound-variable-connected */ ConjunctiveQuery boundVariableConnectedSubquery,
             final String intentionalPredicatePrefix
     ) {
         final var queryFreeVariables = boundVariableConnectedSubquery.getFreeVariables();
@@ -37,21 +35,96 @@ public record GuardedRuleAndQueryRewriter(AbstractSaturation<? extends GTGD> sat
                 intentionalPredicatePrefix + "_GOAL",
                 queryFreeVariables.length
         );
+        final var subqueryGoalAtom = Atom.create(
+                subqueryGoalPredicate,
+                VariableSetExtensions
+                        .sortBySymbol(Arrays.asList(queryFreeVariables))
+                        .toArray(Term[]::new)
+        );
+
+        // A mapping from a set V of connected variables to the predicate that asserts that
+        // the query has been existentially satisfied for the variables in V at the neighborhood of V.
+        // The function may throw an exception on evaluation if the given set of variables contains free variables.
+        final CachingFunction<ImmutableSet</* subquery-bound */ Variable>, Atom> subgoalAtoms;
+        {
+            final var predicateGeneratingCounter = new AtomicInteger(0);
+            subgoalAtoms = new CachingFunction<>(variableSet -> {
+                final var neighbourhood = ConjunctiveQueryExtensions.neighbourhoodVariables(
+                        boundVariableConnectedSubquery,
+                        variableSet
+                );
+                final var symbol = intentionalPredicatePrefix + "_SGL_" + predicateGeneratingCounter.getAndIncrement();
+                final var subgoalPredicate = Predicate.create(symbol, neighbourhood.size());
+                final var orderedNeighbourhood = VariableSetExtensions.sortBySymbol(neighbourhood);
+                return Atom.create(subgoalPredicate, orderedNeighbourhood.toArray(Term[]::new));
+            });
+        }
 
         // TODO: Use SubqueryEntailmentComputation to compute the subquery entailment
         //       and then map all subquery-entailments to the corresponding subgoals.
         Collection<TGD> subgoalDerivationRules = null;
 
-        // TODO: Generate all "glueing rules" to glue the subgoals into the goal predicate.
-        //       For each vertex set V, we need to generate a rule of the form
-        //         (subquery strongly induced by V) ∧ (subgoals for all connected components of (query\V))
-        //           → subqueryGoalPredicate(V)
-        Collection<TGD> subgoalGlueingRules = null;
+        final Collection<TGD> subgoalGlueingRules;
+        {
+            final java.util.function.Predicate<Predicate> isSubgoalPredicate = (Predicate predicate) ->
+                    predicate.getName().startsWith(intentionalPredicatePrefix + "_SGL_");
+
+            subgoalGlueingRules = SetExtensions
+                    .powerset(Arrays.asList(boundVariableConnectedSubquery.getBoundVariables()))
+                    .<TGD>map(existentialWitnessCandidate -> {
+                        // A single existentialWitnessCandidate is a set of variables that the rule
+                        // (which we are about to produce) expects to be existentially satisfied.
+                        //
+                        // We call the complement of existentialWitnessCandidate as baseWitnessVariables,
+                        // since we expect (within the rule we are about to produce) those variables to be witnessed
+                        // by values in the base instance.
+                        //
+                        // The rule that we need to produce, therefore, will be of the form
+                        //   (subquery strongly induced by baseWitnessVariables,
+                        //    except there is no existential quantification)
+                        // ∧ (for each connected component V of existentialWitnessCandidate,
+                        //    a subgoal atom corresponding to V)
+                        //  → subqueryGoalAtom
+                        //
+                        // In the following code, we call the first conjunct of the rule "baseWitnessJoinConditions",
+                        // the second conjunct "neighbourhoodsSubgoals".
+
+                        final var baseWitnessVariables = SetExtensions.difference(
+                                existentialWitnessCandidate,
+                                SetExtensions.union(
+                                        Arrays.asList(boundVariableConnectedSubquery.getBoundVariables()),
+                                        Arrays.asList(boundVariableConnectedSubquery.getFreeVariables())
+                                )
+                        );
+
+                        final var baseWitnessJoinConditions = ConjunctiveQueryExtensions
+                                .strictlyInduceSubqueryByVariables(
+                                        boundVariableConnectedSubquery,
+                                        baseWitnessVariables
+                                ).getAtoms();
+
+                        final var neighbourhoodsSubgoals = ConjunctiveQueryExtensions
+                                .connectedComponents(
+                                        boundVariableConnectedSubquery,
+                                        existentialWitnessCandidate
+                                )
+                                .map(subgoalAtoms)
+                                .toArray(Atom[]::new);
+
+                        return new DatalogRule(
+                                Stream.concat(
+                                        Arrays.stream(baseWitnessJoinConditions),
+                                        Arrays.stream(neighbourhoodsSubgoals)
+                                ).toArray(Atom[]::new),
+                                new Atom[]{subqueryGoalAtom}
+                        );
+                    })
+                    .toList();
+        }
 
         return new BoundVariableConnectedComponentRewriteResult(
-                ImmutableSet.<TGD>builder().addAll(subgoalDerivationRules).addAll(subgoalGlueingRules).build(),
-                subqueryGoalPredicate,
-                ImmutableList.copyOf(queryFreeVariables)
+                SetExtensions.union(subgoalDerivationRules, subgoalGlueingRules),
+                subqueryGoalAtom
         );
     }
 
