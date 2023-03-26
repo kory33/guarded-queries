6ef8110a00654c4f205021866d68fb3ff860864c
Kory <korygm33@gmail.com>
2023-03-26 02:15:46 +0100

    feat: implement set powerset

--- lib/src/main/java/io/github/kory33/guardedqueries/core/utils/extensions/SetExtensions.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/utils/extensions/SetExtensions.java
@@ -2,16 +2,61 @@ package io.github.kory33.guardedqueries.core.utils.extensions;
 
 import com.google.common.collect.ImmutableSet;
 
-import java.util.Set;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class SetExtensions {
     private SetExtensions() {
     }
 
     /**
-     * Intersection of two sets.
+     * Intersection of elements from two collections.
      */
-    public static <T> ImmutableSet<T> intersection(final Set<? extends T> set1, final Set<? extends T> set2) {
-        return ImmutableSet.copyOf(set1.stream().filter(set2::contains).iterator());
+    public static <T> ImmutableSet<T> intersection(
+            final Collection<? extends T> collection1,
+            final Collection<? extends T> collection2
+    ) {
+        final var set2 = ImmutableSet.copyOf(collection2);
+        return ImmutableSet.copyOf(collection1.stream().filter(set2::contains).iterator());
+    }
+
+    /**
+     * Powerset of a set of elements from the given collection, lazily streamed.
+     */
+    public static <T> Stream<ImmutableSet<T>> powerset(final Collection<? extends T> collection) {
+        // deduplicated ArrayList of elements
+        final var arrayList = new ArrayList<>(ImmutableSet.copyOf(collection));
+        final var setSize = arrayList.size();
+
+        // all non-negative BigInteger less than this value represents a unique subset of the given collection
+        final var upperLimit = BigInteger.ONE.shiftLeft(setSize);
+
+        final Iterator<ImmutableSet<T>> iterator = new Iterator<ImmutableSet<T>>() {
+            BigInteger currentIndex = BigInteger.ZERO;
+
+            @Override
+            public boolean hasNext() {
+                // currentIndex < upperLimit
+                return currentIndex.compareTo(upperLimit) < 0;
+            }
+
+            @Override
+            public ImmutableSet<T> next() {
+                final var subset = ImmutableSet.<T>copyOf(
+                        IntStream.range(0, setSize)
+                                .filter(i -> currentIndex.testBit(i))
+                                .mapToObj(arrayList::get)
+                                .iterator()
+                );
+                currentIndex = currentIndex.add(BigInteger.ONE);
+                return subset;
+            }
+        };
+
+        return IteratorExtensions.stream(iterator);
     }
 }
