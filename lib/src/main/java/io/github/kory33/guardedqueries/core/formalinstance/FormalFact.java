f386da3b85d5c58eaac10a0a123c19c7048b65f5
Kory <korygm33@gmail.com>
2023-03-27 14:46:00 +0100

    feat: allow mapping FormalInstance/FormalFact and conversion into atoms

--- lib/src/main/java/io/github/kory33/guardedqueries/core/formalinstance/FormalFact.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/formalinstance/FormalFact.java
@@ -1,9 +1,25 @@
 package io.github.kory33.guardedqueries.core.formalinstance;
 
+import com.google.common.collect.ImmutableList;
+import uk.ac.ox.cs.pdq.fol.Atom;
 import uk.ac.ox.cs.pdq.fol.Predicate;
+import uk.ac.ox.cs.pdq.fol.Term;
 
-public record FormalFact<TermAlphabet>(Predicate predicate, TermAlphabet... appliedTerms) {
+import java.util.function.Function;
+
+public record FormalFact<TermAlphabet>(Predicate predicate, ImmutableList<? extends TermAlphabet> appliedTerms) {
     @SafeVarargs
     public FormalFact {
+    }
+
+    public <T> FormalFact<T> map(final Function<? super TermAlphabet, ? extends T> mapper) {
+        return new FormalFact<>(
+                this.predicate,
+                ImmutableList.copyOf(this.appliedTerms.stream().map(mapper).iterator())
+        );
+    }
+
+    public static Atom asAtom(final FormalFact<Term> fact) {
+        return Atom.create(fact.predicate, fact.appliedTerms.toArray(Term[]::new));
     }
 }
--- lib/src/main/java/io/github/kory33/guardedqueries/core/formalinstance/FormalInstance.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/formalinstance/FormalInstance.java
@@ -1,11 +1,46 @@
 package io.github.kory33.guardedqueries.core.formalinstance;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import uk.ac.ox.cs.pdq.fol.Atom;
+import uk.ac.ox.cs.pdq.fol.Term;
+
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.function.Function;
 
 public class FormalInstance<TermAlphabet> {
-    protected final Collection<FormalFact<TermAlphabet>> facts;
+    public final ImmutableSet<FormalFact<TermAlphabet>> facts;
+    private ImmutableSet<TermAlphabet> activeTerms;
 
-    protected FormalInstance(final Collection<FormalFact<TermAlphabet>> facts) {
-        this.facts = facts;
+    public FormalInstance(final Collection<FormalFact<TermAlphabet>> facts) {
+        this.facts = ImmutableSet.copyOf(facts);
+    }
+
+    public FormalInstance(final Iterator<FormalFact<TermAlphabet>> facts) {
+        this.facts = ImmutableSet.copyOf(facts);
+    }
+
+    public ImmutableSet<TermAlphabet> getActiveTerms() {
+        if (this.activeTerms == null) {
+            this.activeTerms = ImmutableSet.copyOf(this.facts
+                    .stream()
+                    .flatMap(fact -> fact.appliedTerms().stream())
+                    .iterator()
+            );
+        }
+        return this.activeTerms;
+    }
+
+    public <T> FormalInstance<T> map(final Function<TermAlphabet, T> mapper) {
+        return new FormalInstance<>(this.facts.stream().map(fact -> fact.map(mapper)).iterator());
+    }
+
+    public static ImmutableList<Atom> asAtoms(final FormalInstance<Term> instance) {
+        return ImmutableList.copyOf(
+                instance.facts.stream()
+                        .map(FormalFact::asAtom)
+                        .iterator()
+        );
     }
 }
