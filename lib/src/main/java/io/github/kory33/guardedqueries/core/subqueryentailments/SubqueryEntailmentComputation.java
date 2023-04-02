cd547981cfbe8505654a335ab90776d295ff369c
Kory <korygm33@gmail.com>
2023-04-02 22:02:20 +0100

    fix: fix the well-formed rule about queryConstantEmbedding in the return values of SubqueryEntailmentComputation

--- lib/src/main/java/io/github/kory33/guardedqueries/core/subqueryentailments/SubqueryEntailmentComputation.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/subqueryentailments/SubqueryEntailmentComputation.java
@@ -48,8 +48,16 @@ import java.util.stream.Stream;
  *     to local names appearing in {@code i.localInstance()}
  *    </li>
  *    <li>
- *     {@code i.queryConstantEmbedding()} is an injective map sending some local names
- *     that are not in {@code i.localWitnessGuess().values()} to constants appearing in {@code Q} but not in {@code S}
+ *     {@code i.queryConstantEmbedding()} is an injective map sending some local names that are
+ *     <ol>
+ *      <li>active in {@code i.localInstance()} and</li>
+ *      <li>not in the range of {@code i.localWitnessGuess()}</li>
+ *     </ol>
+ *     to constants that
+ *     <ol>
+ *      <li>appears in a subquery of {@code Q} weakly induced by {@code i.coexistentialVariables()} but</li>
+ *      <li>does not appear in {@code S}</li>
+ *     </ol>
  *    </li>
  *   </ol>
  *  </li>
