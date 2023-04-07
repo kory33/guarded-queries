46ce5328c1d472c4013c859a35cff2b243a424a7
Kory <korygm33@gmail.com>
2023-04-07 13:32:26 +0100

    feat: implement NaiveDPTableSEComputation::allLocalInstances

--- lib/src/main/java/io/github/kory33/guardedqueries/core/subqueryentailments/computationimpls/NaiveDPTableSEComputation.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/subqueryentailments/computationimpls/NaiveDPTableSEComputation.java
@@ -1,29 +1,38 @@
 package io.github.kory33.guardedqueries.core.subqueryentailments.computationimpls;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import io.github.kory33.guardedqueries.core.datalog.DatalogEngine;
 import io.github.kory33.guardedqueries.core.fol.FunctionFreeSignature;
 import io.github.kory33.guardedqueries.core.fol.NormalGTGD;
+import io.github.kory33.guardedqueries.core.formalinstance.FormalFact;
 import io.github.kory33.guardedqueries.core.formalinstance.FormalInstance;
 import io.github.kory33.guardedqueries.core.rewriting.SaturatedRuleSet;
 import io.github.kory33.guardedqueries.core.subqueryentailments.LocalInstanceTerm;
 import io.github.kory33.guardedqueries.core.subqueryentailments.SubqueryEntailmentComputation;
 import io.github.kory33.guardedqueries.core.subqueryentailments.SubqueryEntailmentInstance;
-import io.github.kory33.guardedqueries.core.utils.extensions.ConjunctiveQueryExtensions;
-import io.github.kory33.guardedqueries.core.utils.extensions.ImmutableMapExtensions;
-import io.github.kory33.guardedqueries.core.utils.extensions.SetLikeExtensions;
-import io.github.kory33.guardedqueries.core.utils.extensions.StreamExtensions;
+import io.github.kory33.guardedqueries.core.utils.extensions.*;
 import uk.ac.ox.cs.pdq.fol.ConjunctiveQuery;
 import uk.ac.ox.cs.pdq.fol.Constant;
+import uk.ac.ox.cs.pdq.fol.Predicate;
 import uk.ac.ox.cs.pdq.fol.Variable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static io.github.kory33.guardedqueries.core.utils.MappingStreams.*;
 
 public final class NaiveDPTableSEComputation implements SubqueryEntailmentComputation {
-    private static final class DPTable {
+    private final DatalogEngine datalogEngine;
+
+    public NaiveDPTableSEComputation(final DatalogEngine datalogEngine) {
+        this.datalogEngine = datalogEngine;
+    }
+
+    private final class DPTable {
         private final HashMap<SubqueryEntailmentInstance, Boolean> table = new HashMap<>();
         private final SaturatedRuleSet<? extends NormalGTGD> saturatedRuleSet;
         private final FunctionFreeSignature folSignature;
@@ -132,11 +141,62 @@ public final class NaiveDPTableSEComputation implements SubqueryEntailmentComput
         }
     }
 
-    private static Stream<FormalInstance<LocalInstanceTerm>> allLocalInstancesOver(
+    private static Stream<FormalInstance<LocalInstanceTerm>> allLocalInstances(
             final FunctionFreeSignature signature,
             final ImmutableSet<Constant> ruleConstants
     ) {
-        throw new RuntimeException("Not implemented yet");
+        final var maxArity = signature.maxArity();
+        final var ruleConstantsAsLocalTerms = ImmutableSet.copyOf(
+                ruleConstants.stream().map(LocalInstanceTerm.RuleConstant::new).iterator()
+        );
+
+        final var allActiveLocalNames = SetLikeExtensions
+                .powerset(IntStream.range(0, maxArity * 2).boxed().toList())
+                .filter(localNameSet -> localNameSet.size() == maxArity);
+
+        return allActiveLocalNames.flatMap(localNameSet -> {
+            final var localNames = ImmutableSet.copyOf(
+                    localNameSet.stream().map(LocalInstanceTerm.LocalName::new).iterator()
+            );
+            final var allLocalInstanceTerms = SetLikeExtensions.union(localNames, ruleConstantsAsLocalTerms);
+            final var predicateList = signature.predicates().stream().toList();
+
+            final Function<Predicate, Iterable<FormalInstance<LocalInstanceTerm>>> allLocalInstancesOverThePredicate = predicate -> {
+                final var predicateParameterIndices = IntStream.range(0, predicate.getArity()).boxed().toList();
+                final var allFormalFactsOverThePredicate = allTotalFunctionsBetween(
+                        predicateParameterIndices,
+                        allLocalInstanceTerms
+                ).map(parameterMap -> {
+                    final var parameterList = ImmutableList.copyOf(
+                            IntStream
+                                    .range(0, predicate.getArity())
+                                    .mapToObj(parameterMap::get)
+                                    .iterator()
+                    );
+
+                    return new FormalFact<LocalInstanceTerm>(predicate, parameterList);
+                });
+
+                return () -> SetLikeExtensions
+                        .powerset(allFormalFactsOverThePredicate.toList())
+                        .map(FormalInstance::new)
+                        .iterator();
+            };
+
+            final var allInstancesOverLocalNameSet = IteratorExtensions.mapInto(
+                    ListExtensions
+                            .productMappedCollectionsToSets(predicateList, allLocalInstancesOverThePredicate)
+                            .iterator(),
+                    FormalInstance::unionAll
+            );
+
+            return IteratorExtensions
+                    .intoStream(allInstancesOverLocalNameSet)
+                    .filter(instance -> {
+                        final var activeLocalNames = instance.getActiveTermsInClass(LocalInstanceTerm.LocalName.class);
+                        return activeLocalNames.size() == localNameSet.size();
+                    });
+        });
     }
 
     private static Stream<SubqueryEntailmentInstance> allWellFormedSubqueryEntailmentInstancesFor(
@@ -146,11 +206,8 @@ public final class NaiveDPTableSEComputation implements SubqueryEntailmentComput
     ) {
         final var queryVariables = ImmutableSet.copyOf(conjunctiveQuery.getVariablesRecursive());
 
-        final var allLocalInstances = allLocalInstancesOver(signature, ruleConstants);
-        final var allRuleConstantWitnessGuesses = allPartialFunctionsBetween(queryVariables, ruleConstants);
-
-        return allRuleConstantWitnessGuesses.flatMap(ruleConstantWitnessGuess ->
-                allLocalInstances.flatMap(localInstance -> {
+        return allPartialFunctionsBetween(queryVariables, ruleConstants).flatMap(ruleConstantWitnessGuess ->
+                allLocalInstances(signature, ruleConstants).flatMap(localInstance -> {
                     final var allCoexistentialVariableSets = SetLikeExtensions
                             .powerset(queryVariables)
                             .filter(variableSet -> SetLikeExtensions.disjoint(variableSet, ruleConstantWitnessGuess.keySet()))
@@ -201,7 +258,18 @@ public final class NaiveDPTableSEComputation implements SubqueryEntailmentComput
         final var ruleConstants = saturatedRuleSet.rules.getConstants();
 
         final var dpTable = new DPTable(saturatedRuleSet, signature, conjunctiveQuery);
+
+        // NOTE: This is massively inefficient because we only have to consider local instances
+        //       that are both guarded and saturated by the rules.
+        //       Moreover, once we mark a local instance (together with rules constant witness guess and other data)
+        //       as a true instance, we no longer have to fill the table for larger instances (due to subsumptions).
+        //       Finally, we could always "normalize" instances so that active values are always within the range of
+        //       {0,1,...,maxArity-1} (except during the chasing phase), so that we do not have to explore
+        //       all possible local instances, but only those that are normalized.
+        //       However, in this NaiveDPTable implementation, we do not optimize the DP table for subsumption or
+        //       normalization, so we are just filling the table for all well-formed instances.
         allWellFormedSubqueryEntailmentInstancesFor(signature, ruleConstants, conjunctiveQuery).forEach(dpTable::fillTableUpto);
+
         return dpTable.getKnownYesInstances();
     }
 }
