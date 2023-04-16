eaf763bc6f8e110a1838a36fd37188175f16bb7c
Kory <korygm33@gmail.com>
2023-04-16 19:47:53 +0100

    feat: allow printing DatalogProgram

--- lib/src/main/java/io/github/kory33/guardedqueries/core/datalog/DatalogProgram.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/datalog/DatalogProgram.java
@@ -1,5 +1,6 @@
 package io.github.kory33.guardedqueries.core.datalog;
 
+import com.google.common.collect.ImmutableList;
 import io.github.kory33.guardedqueries.core.fol.DatalogRule;
 import uk.ac.ox.cs.pdq.fol.Dependency;
 
@@ -8,5 +9,10 @@ import java.util.Collection;
 public record DatalogProgram(Collection<DatalogRule> rules) {
     public static DatalogProgram tryFromDependencies(final Collection<? extends Dependency> dependencies) {
         return new DatalogProgram(dependencies.stream().map(DatalogRule::tryFromDependency).toList());
+    }
+
+    @Override
+    public String toString() {
+        return ImmutableList.copyOf(rules).toString();
     }
 }
