37f9bf1d9a4f3c05385d8a3d42d93ab130fb1f3f
Kory <korygm33@gmail.com>
2023-04-19 18:12:03 +0100

    fix: make tests actually run with gradle test

--- lib-test/build.gradle.kts
+++ lib-test/build.gradle.kts
@@ -8,7 +8,8 @@ repositories {
 
 dependencies {
     testImplementation("org.scala-lang:scala3-library_3:3.2.2")
-    testImplementation("org.scalacheck:scalacheck_3:1.17.0")
+    testImplementation("org.scalatestplus:scalacheck-1-17_3:3.2.15.0")
+    testImplementation("org.scalatest:scalatest-flatspec_3:3.2.15")
 
     testImplementation(project(":lib"))
     testImplementation(project(mapOf(
@@ -20,4 +21,20 @@ dependencies {
 tasks.withType<ScalaCompile>().configureEach {
     sourceCompatibility = "1.17"
     targetCompatibility = "1.17"
+}
+
+// credit: https://stackoverflow.com/a/27096395
+tasks.register("spec", JavaExec::class.java) {
+    dependsOn("testClasses")
+    mainClass.set("org.scalatest.tools.Runner")
+    classpath = sourceSets["test"].runtimeClasspath
+    args = listOf(
+            "-R", "build/classes/scala/test",
+            // show Duration and Full stacktrace
+            "-oDF"
+    )
+}
+
+tasks.test {
+    dependsOn("spec")
 }
