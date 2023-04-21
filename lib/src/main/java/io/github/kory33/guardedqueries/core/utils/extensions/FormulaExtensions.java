d95e67cc1140d3e428474ad04243c7459711f346
Kory <korygm33@gmail.com>
2023-04-21 08:32:12 +0100

    fix: throw if the formula contains anything other than atoms (e.g. equalities?) as leaf formula

--- lib/src/main/java/io/github/kory33/guardedqueries/core/utils/extensions/FormulaExtensions.java
+++ lib/src/main/java/io/github/kory33/guardedqueries/core/utils/extensions/FormulaExtensions.java
@@ -14,10 +14,10 @@ public class FormulaExtensions {
         final var children = formula.getChildren();
 
         if (children.length == 0) {
-            if (formula instanceof Atom) {
-                return Stream.of(((Atom) formula).getPredicate());
+            if (formula instanceof Atom atom) {
+                return Stream.of(atom.getPredicate());
             } else {
-                return Stream.empty();
+                throw new IllegalArgumentException("Formula " + formula + "is neither an atom nor a composite formula");
             }
         } else {
             return Stream.of(children).flatMap(FormulaExtensions::streamPredicatesAppearingIn);
