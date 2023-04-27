4c1e2e80429d5e2d897e17858256684809f3f9e5
Kory <korygm33@gmail.com>
2023-04-27 21:13:23 +0100

    refactor: separate out the test harness from NaiveDPTableSEComputationTests

--- lib-integration-tests/src/test/java/io/github/kory33/guardedqueries/core/gsatequivalencetests/NaiveDPTableSEComputationTests.java
+++ lib-integration-tests/src/test/java/io/github/kory33/guardedqueries/core/gsatequivalencetests/NaiveDPTableSEComputationTests.java
@@ -1,247 +1,63 @@
 package io.github.kory33.guardedqueries.core.gsatequivalencetests;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import io.github.kory33.guardedqueries.core.datalog.DatalogProgram;
-import io.github.kory33.guardedqueries.core.datalog.DatalogRewriteResult;
 import io.github.kory33.guardedqueries.core.datalog.saturationengines.NaiveSaturationEngine;
-import io.github.kory33.guardedqueries.core.fol.FunctionFreeSignature;
-import io.github.kory33.guardedqueries.core.formalinstance.FormalFact;
-import io.github.kory33.guardedqueries.core.formalinstance.FormalInstance;
-import io.github.kory33.guardedqueries.core.formalinstance.joins.naturaljoinalgorithms.FilterNestedLoopJoin;
 import io.github.kory33.guardedqueries.core.rewriting.GuardedRuleAndQueryRewriter;
 import io.github.kory33.guardedqueries.core.subqueryentailments.computationimpls.NaiveDPTableSEComputation;
-import io.github.kory33.guardedqueries.core.subsumption.formula.MinimalExactBodyDatalogRuleSet;
-import io.github.kory33.guardedqueries.core.subsumption.formula.MinimallyUnifiedDatalogRuleSet;
-import io.github.kory33.guardedqueries.core.testcases.GTGDRuleAndGTGDReducibleQuery;
 import io.github.kory33.guardedqueries.core.testcases.GTGDRuleAndGTGDReducibleQueryTestCases;
-import io.github.kory33.guardedqueries.core.utils.MappingStreams;
+import io.github.kory33.guardedqueries.core.testharnesses.GSatEquivalenceTestHarness;
 import org.junit.jupiter.api.Test;
 import uk.ac.ox.cs.gsat.GSat;
-import uk.ac.ox.cs.pdq.fol.*;
-
-import java.time.Instant;
-import java.util.Date;
-import java.util.stream.IntStream;
 
 public class NaiveDPTableSEComputationTests {
-    public record RewriteResultsToBeCompared(
-            DatalogProgram gsatRewriting,
-            ConjunctiveQuery gsatQuery,
-            DatalogRewriteResult ourRewriting,
-
-            // the atom to which we can materialize computed homomorphisms
-            Atom answerAtom
-    ) {
-        public FormalInstance<Constant> answersToGsatRewriting(
-                final FormalInstance<Constant> testInstance
-        ) {
-            final var gsatSaturatedInstance = new NaiveSaturationEngine()
-                    .saturateInstance(gsatRewriting, testInstance, c -> c);
-
-            return new FormalInstance<>(
-                    new FilterNestedLoopJoin<>(c -> c)
-                            .join(gsatQuery, gsatSaturatedInstance)
-                            .materializeFunctionFreeAtom(answerAtom, c -> c)
-            );
-        }
-
-        public FormalInstance<Constant> answersToOurRewriting(
-                final FormalInstance<Constant> testInstance
-        ) {
-            final var saturationEngine = new NaiveSaturationEngine();
-
-            // We run the output program in two steps:
-            // We first derive all facts other than subgoal/goals (i.e. with input predicates),
-            // and then derive subgoal / goal facts in one go.
-            // This should be much more efficient than saturating with
-            // all output rules at once, since we can rather quickly saturate the base data with
-            // input rules (output of GSat being relatively small, and after having done that
-            // we only need to go through subgoal derivation rules once.
-            final var inputRuleSaturatedInstance = saturationEngine
-                    .saturateInstance(ourRewriting.inputRuleSaturationRules(), testInstance, c -> c);
-            final var saturatedInstance = saturationEngine
-                    .saturateInstance(ourRewriting.subgoalAndGoalDerivationRules(), inputRuleSaturatedInstance, c -> c);
-
-            final var rewrittenGoalQuery = ConjunctiveQuery.create(
-                    ourRewriting.goal().getVariables(),
-                    new Atom[]{ourRewriting.goal()}
-            );
-
-            return new FormalInstance<>(
-                    new FilterNestedLoopJoin<>(c -> c)
-                            .join(rewrittenGoalQuery, saturatedInstance)
-                            .materializeFunctionFreeAtom(answerAtom, c -> c)
-            );
-        }
-    }
-
-    public RewriteResultsToBeCompared rewriteInTwoMethods(
-            final GTGDRuleAndGTGDReducibleQuery ruleQuery
-    ) {
-        System.out.println("[" + Date.from(Instant.now()) + "] Rewriting " + ruleQuery.reducibleQuery().originalQuery());
-
-        final var gsatRewriting = DatalogProgram.tryFromDependencies(
-                GSat.getInstance().run(
-                        ImmutableList.<Dependency>builder()
-                                .addAll(ruleQuery.guardedRules())
-                                .addAll(ruleQuery.reducibleQuery().reductionRules())
-                                .build()
-                )
-        );
-        final var gsatQuery = ruleQuery.reducibleQuery().existentialFreeQuery();
-
-        System.out.println("[" + Date.from(Instant.now()) + "] Done Gsat rewriting");
-
-        final var ourRewriting = new GuardedRuleAndQueryRewriter(
-                GSat.getInstance(),
-                new NaiveDPTableSEComputation(new NaiveSaturationEngine())
-        ).rewrite(ruleQuery.guardedRules(), ruleQuery.reducibleQuery().originalQuery());
-
-        System.out.println("[" + Date.from(Instant.now()) + "] Done guarded-query rewriting");
-        System.out.println("[" + Date.from(Instant.now()) + "] # of subgoal derivation rules in original output: " +
-                ourRewriting.subgoalAndGoalDerivationRules().rules().size());
-
-        final var deduplicatedFreeVariablesInQuery = ImmutableList.copyOf(ImmutableSet.copyOf(ruleQuery
-                .reducibleQuery()
-                .existentialFreeQuery()
-                .getFreeVariables()
-        ));
-
-        final var answerAtom = Atom.create(
-                Predicate.create("Answer", deduplicatedFreeVariablesInQuery.size()),
-                deduplicatedFreeVariablesInQuery.toArray(Variable[]::new)
-        );
-
-        final var minimalExactBodyMinimizedRewriting = ourRewriting
-                .minimizeSubgoalDerivationRulesUsing(MinimalExactBodyDatalogRuleSet::new);
-
-        System.out.println("[" + Date.from(Instant.now()) + "] # of subgoal derivation rules in minimalExactBodyMinimizedRewriting: " +
-                minimalExactBodyMinimizedRewriting.subgoalAndGoalDerivationRules().rules().size());
-
-        final var minimizedRewriting = minimalExactBodyMinimizedRewriting
-                .minimizeSubgoalDerivationRulesUsing(MinimallyUnifiedDatalogRuleSet::new);
-
-        System.out.println("[" + Date.from(Instant.now()) + "] # of subgoal derivation rules in minimizedRewriting: " +
-                minimizedRewriting.subgoalAndGoalDerivationRules().rules().size());
-
-        return new RewriteResultsToBeCompared(
-                gsatRewriting,
-                gsatQuery,
-                minimizedRewriting,
-                answerAtom
-        );
-    }
-
-    private FormalInstance<Constant> allFactsOver(
-            final Predicate predicate,
-            final ImmutableSet<Constant> constantsToUse
-    ) {
-        final var predicateArgIndices = ImmutableList.copyOf(
-                IntStream.range(0, predicate.getArity()).iterator()
-        );
-
-        final var allFormalFacts = MappingStreams
-                .allTotalFunctionsBetween(predicateArgIndices, constantsToUse)
-                .map(mapping -> new FormalFact<>(
-                        predicate,
-                        ImmutableList.copyOf(predicateArgIndices.stream().map(mapping::get).iterator())
-                ));
-
-        return new FormalInstance<>(allFormalFacts.iterator());
-    }
-
-    private FormalInstance<Constant> randomInstanceOver(final FunctionFreeSignature signature) {
-        final var constantsToUse = ImmutableSet.copyOf(
-                IntStream.range(0, signature.maxArity() * 4)
-                        .<Constant>mapToObj(i -> TypedConstant.create("c_" + i))
-                        .iterator()
-        );
-
-        final var allFactsOverSignature =
-                signature.predicates().stream().flatMap(p -> allFactsOver(p, constantsToUse).facts.stream());
-
-        // we first decide a selection rate and use it as a threshold
-        // to filter out some of the tuples in the instance
-        final var selectionRate = Math.pow(Math.random(), 2.5);
-
-        return new FormalInstance<>(
-                allFactsOverSignature
-                        .filter(fact -> Math.random() < selectionRate)
-                        .iterator()
-        );
-    }
-
-    private void runTestFor(
-            final GTGDRuleAndGTGDReducibleQuery ruleQuery,
-            final int testIterationCount
-    ) {
-        final var rewritings = rewriteInTwoMethods(ruleQuery);
-
-        for (int i = 0; i < testIterationCount; i++) {
-            final var testInstance = randomInstanceOver(ruleQuery.signatureOfOriginalQuery());
-            final var gsatAnswer = rewritings.answersToGsatRewriting(testInstance);
-            final var ourAnswer = rewritings.answersToOurRewriting(testInstance);
-
-            if (!gsatAnswer.equals(ourAnswer)) {
-                throw new AssertionError(
-                        "GSat and our answer differ! " +
-                                "input = " + testInstance + ", " +
-                                "gsatAnswer: " + gsatAnswer + ", " +
-                                "ourAnswer: " + ourAnswer
-                );
-            } else {
-                System.out.println(
-                        "[" + Date.from(Instant.now()) + "] Test " + i + " passed, " +
-                                "input size = " + testInstance.facts.size() + ", " +
-                                "answer size = " + gsatAnswer.facts.size()
-                );
-            }
-        }
-    }
+    private static final GSatEquivalenceTestHarness harness = new GSatEquivalenceTestHarness(
+            GSat.getInstance(),
+            new GuardedRuleAndQueryRewriter(
+                    GSat.getInstance(), new NaiveDPTableSEComputation(new NaiveSaturationEngine())
+            )
+    );
 
     @Test
     public void testEquivalenceOn__SimpleArity2Rule_0__atomicQuery() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.atomicQuery, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.atomicQuery, 4000);
     }
 
     @Test
     public void testEquivalenceOn__SimpleArity2Rule_0__joinQuery() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.joinQuery, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.joinQuery, 4000);
     }
 
     @Test
     public void testEquivalenceOn__SimpleArity2Rule_0__existentialGuardedQuery_0() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.existentialGuardedQuery_0, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.existentialGuardedQuery_0, 4000);
     }
 
     @Test
     public void testEquivalenceOn__SimpleArity2Rule_0__existentialJoinQuery_0() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.existentialJoinQuery_0, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.existentialJoinQuery_0, 4000);
     }
 
     @Test
     public void testEquivalenceOn__SimpleArity2Rule_0__existentialJoinQuery_1() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.existentialJoinQuery_1, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.SimpleArity2Rule_0.existentialJoinQuery_1, 4000);
     }
 
     @Test
     public void testEquivalenceOn__Arity4Rule__atomicQuery() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.Arity4Rule.atomicQuery, 30);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.Arity4Rule.atomicQuery, 30);
     }
 
     @Test
     public void testEquivalenceOn__ConstantRule__atomicQuery() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.ConstantRule.atomicQuery, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.ConstantRule.atomicQuery, 4000);
     }
 
     @Test
     public void testEquivalenceOn__ConstantRule__existentialBooleanQueryWithConstant() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.ConstantRule.existentialBooleanQueryWithConstant, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.ConstantRule.existentialBooleanQueryWithConstant, 4000);
     }
 
     @Test
     public void testEquivalenceOn__ConstantRule__existentialGuardedWithConstant() {
-        runTestFor(GTGDRuleAndGTGDReducibleQueryTestCases.ConstantRule.existentialGuardedWithConstant, 4000);
+        harness.runTestOn(GTGDRuleAndGTGDReducibleQueryTestCases.ConstantRule.existentialGuardedWithConstant, 4000);
     }
 }
---
+++ lib-integration-tests/src/test/java/io/github/kory33/guardedqueries/core/testharnesses/GSatEquivalenceTestHarness.java
@@ -0,0 +1,173 @@
+package io.github.kory33.guardedqueries.core.testharnesses;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import io.github.kory33.guardedqueries.core.datalog.DatalogProgram;
+import io.github.kory33.guardedqueries.core.datalog.DatalogRewriteResult;
+import io.github.kory33.guardedqueries.core.datalog.saturationengines.NaiveSaturationEngine;
+import io.github.kory33.guardedqueries.core.formalinstance.FormalInstance;
+import io.github.kory33.guardedqueries.core.formalinstance.joins.naturaljoinalgorithms.FilterNestedLoopJoin;
+import io.github.kory33.guardedqueries.core.rewriting.GuardedRuleAndQueryRewriter;
+import io.github.kory33.guardedqueries.core.subsumption.formula.MinimalExactBodyDatalogRuleSet;
+import io.github.kory33.guardedqueries.core.subsumption.formula.MinimallyUnifiedDatalogRuleSet;
+import io.github.kory33.guardedqueries.core.testcases.GTGDRuleAndGTGDReducibleQuery;
+import uk.ac.ox.cs.gsat.AbstractSaturation;
+import uk.ac.ox.cs.gsat.GTGD;
+import uk.ac.ox.cs.pdq.fol.*;
+
+import java.time.Instant;
+import java.util.Date;
+
+import static io.github.kory33.guardedqueries.core.testharnesses.InstanceGeneration.randomInstanceOver;
+
+/**
+ * A test harness to compare the rewriting outputs of GSat and of our implementation
+ * on {@link GTGDRuleAndGTGDReducibleQuery} instances.
+ */
+public record GSatEquivalenceTestHarness(AbstractSaturation<? extends GTGD> gsatImplementation,
+                                         GuardedRuleAndQueryRewriter rewriterToBeTested) {
+
+    private static void logWithTime(String message) {
+        System.out.println("[" + Date.from(Instant.now()) + "] " + message);
+    }
+
+    private record RewriteResultsToBeCompared(
+            DatalogProgram gsatRewriting,
+            ConjunctiveQuery gsatQuery,
+            DatalogRewriteResult ourRewriting,
+
+            // the atom to which we can materialize computed homomorphisms
+            Atom answerAtom
+    ) {
+        public FormalInstance<Constant> answersWithGsatRewriting(
+                final FormalInstance<Constant> testInstance
+        ) {
+            final var gsatSaturatedInstance = new NaiveSaturationEngine()
+                    .saturateInstance(gsatRewriting, testInstance, c -> c);
+
+            return new FormalInstance<>(
+                    new FilterNestedLoopJoin<>(c -> c)
+                            .join(gsatQuery, gsatSaturatedInstance)
+                            .materializeFunctionFreeAtom(answerAtom, c -> c)
+            );
+        }
+
+        public FormalInstance<Constant> answersWithOurRewriting(
+                final FormalInstance<Constant> testInstance
+        ) {
+            final var saturationEngine = new NaiveSaturationEngine();
+
+            // We run the output program in two steps:
+            // We first derive all facts other than subgoal/goals (i.e. with input predicates),
+            // and then derive subgoal / goal facts in one go.
+            // This should be much more efficient than saturating with
+            // all output rules at once, since we can rather quickly saturate the base data with
+            // input rules (output of GSat being relatively small, and after having done that
+            // we only need to go through subgoal derivation rules once.
+            final var inputRuleSaturatedInstance = saturationEngine
+                    .saturateInstance(ourRewriting.inputRuleSaturationRules(), testInstance, c -> c);
+            final var saturatedInstance = saturationEngine
+                    .saturateInstance(ourRewriting.subgoalAndGoalDerivationRules(), inputRuleSaturatedInstance, c -> c);
+
+            final var rewrittenGoalQuery = ConjunctiveQuery.create(
+                    ourRewriting.goal().getVariables(),
+                    new Atom[]{ourRewriting.goal()}
+            );
+
+            return new FormalInstance<>(
+                    new FilterNestedLoopJoin<>(c -> c)
+                            .join(rewrittenGoalQuery, saturatedInstance)
+                            .materializeFunctionFreeAtom(answerAtom, c -> c)
+            );
+        }
+    }
+
+    private RewriteResultsToBeCompared rewriteInTwoMethods(
+            final GTGDRuleAndGTGDReducibleQuery ruleQuery
+    ) {
+        logWithTime("Rewriting " + ruleQuery.reducibleQuery().originalQuery());
+
+        final var gsatRewriting = DatalogProgram.tryFromDependencies(
+                gsatImplementation.run(
+                        ImmutableList.<Dependency>builder()
+                                .addAll(ruleQuery.guardedRules())
+                                .addAll(ruleQuery.reducibleQuery().reductionRules())
+                                .build()
+                )
+        );
+        final var gsatQuery = ruleQuery.reducibleQuery().existentialFreeQuery();
+
+        logWithTime("Done Gsat rewriting");
+
+        final var ourRewriting = rewriterToBeTested
+                .rewrite(ruleQuery.guardedRules(), ruleQuery.reducibleQuery().originalQuery());
+
+        logWithTime("Done guarded-query rewriting");
+        logWithTime("# of subgoal derivation rules in original output: " +
+                ourRewriting.subgoalAndGoalDerivationRules().rules().size());
+
+        final var deduplicatedFreeVariablesInQuery = ImmutableList.copyOf(ImmutableSet.copyOf(ruleQuery
+                .reducibleQuery()
+                .existentialFreeQuery()
+                .getFreeVariables()
+        ));
+
+        final var answerAtom = Atom.create(
+                Predicate.create("Answer", deduplicatedFreeVariablesInQuery.size()),
+                deduplicatedFreeVariablesInQuery.toArray(Variable[]::new)
+        );
+
+        final var minimalExactBodyMinimizedRewriting = ourRewriting
+                .minimizeSubgoalDerivationRulesUsing(MinimalExactBodyDatalogRuleSet::new);
+
+        logWithTime("# of subgoal derivation rules in minimalExactBodyMinimizedRewriting: " +
+                minimalExactBodyMinimizedRewriting.subgoalAndGoalDerivationRules().rules().size());
+
+        final var minimizedRewriting = minimalExactBodyMinimizedRewriting
+                .minimizeSubgoalDerivationRulesUsing(MinimallyUnifiedDatalogRuleSet::new);
+
+        logWithTime("# of subgoal derivation rules in minimizedRewriting: " +
+                minimizedRewriting.subgoalAndGoalDerivationRules().rules().size());
+
+        return new RewriteResultsToBeCompared(
+                gsatRewriting,
+                gsatQuery,
+                minimizedRewriting,
+                answerAtom
+        );
+    }
+
+    /**
+     * Test that {@link #gsatImplementation} and {@link #rewriterToBeTested} produce
+     * the equivalent Datalog rewritings on the given {@link GTGDRuleAndGTGDReducibleQuery}.
+     * <p>
+     * The test is repeatedly performed on randomly generated database instances
+     * (with the number of test rounds being specified by {@code testInstanceGenerationCount}).
+     */
+    public void runTestOn(
+            final GTGDRuleAndGTGDReducibleQuery ruleQuery,
+            final int testInstanceGenerationCount
+    ) {
+        final var rewritings = rewriteInTwoMethods(ruleQuery);
+
+        for (int i = 0; i < testInstanceGenerationCount; i++) {
+            final var testInstance = randomInstanceOver(ruleQuery.signatureOfOriginalQuery());
+
+            final var gsatAnswer = rewritings.answersWithGsatRewriting(testInstance);
+            final var ourAnswer = rewritings.answersWithOurRewriting(testInstance);
+
+            if (!gsatAnswer.equals(ourAnswer)) {
+                throw new AssertionError("GSat and our answer differ! " +
+                        "input = " + testInstance + ", " +
+                        "gsatAnswer = " + gsatAnswer + ", " +
+                        "ourAnswer = " + ourAnswer
+                );
+            } else {
+                logWithTime("Test " + i + " passed, " +
+                        "input size = " + testInstance.facts.size() + ", " +
+                        "answer size = " + gsatAnswer.facts.size()
+                );
+            }
+        }
+    }
+}
