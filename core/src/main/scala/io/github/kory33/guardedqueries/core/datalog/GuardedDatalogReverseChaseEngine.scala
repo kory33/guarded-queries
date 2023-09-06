741c5f7d7d032979bd8c0bb7fd443672ef491555
Kory <korygm33@gmail.com>
2023-09-06 11:43:05 +0900

    docs: improve the documentation at GuardedDatalogReverseChaseEngine

--- core/src/main/scala/io/github/kory33/guardedqueries/core/datalog/GuardedDatalogReverseChaseEngine.scala
+++ core/src/main/scala/io/github/kory33/guardedqueries/core/datalog/GuardedDatalogReverseChaseEngine.scala
@@ -6,23 +6,24 @@ import io.github.kory33.guardedqueries.core.subqueryentailments.{LocalInstance, 
 /**
  * A trait representing a reverse chase engine for guarded Datalog.
  *
- * Fix `localNamesToFix` and define a binary relation `R` on `LocalInstance` as follows:
- * `instance1 R instance2` if and only if there exists a map `s: TA => TA` such that
- *   - `s(instance2) subsetOf instance1`, and
- *   - `s` is the identity map on `localNamesToFix`.
- *
- * The relation `R` so defined is a preorder on `LocalInstance`. When we have a relation
- * `instance1 R instance2`, we say that "`instance2` is as strong as `instance1` (with respect
- * to `localNamesToFix`)".
+ * Let `R` be the "strength" relation on local instances, as described in
+ * [[io.github.kory33.guardedqueries.core.subsumption.localinstance.MaximallyStrongLocalInstanceSet]].
  *
  * The [[reverseChase]] method returns a finite collection of all (`R`-isomorphism-classes of)
- * `R`-maximal local instances `I` that satisfies `instance R saturate(program, I)`, where
- * `saturate` is [[DatalogSaturationEngine.saturateInstance]].
+ * `R`-maximal local instances `I` that satisfies
+ *   - `instance R saturate(program, I)`, where `saturate` is
+ *     [[DatalogSaturationEngine.saturateInstance]], and
+ *   - the width of `I` (number of local names active in `I`) is at most
+ *     `instanceWidthUpperLimit`.
  */
 trait GuardedDatalogReverseChaseEngine {
+  // TODO (Question):
+  //   Is it OK to take `instanceWidthUpperLimit` to be the maximum arity of predicates
+  //   appearing in `program` and omit this parameter?
   def reverseChase(
     localNamesToFix: Set[LocalName],
     program: GuardedDatalogProgram,
+    instanceWidthUpperLimit: Int,
     instance: LocalInstance
   ): Iterable[LocalInstance]
 }
